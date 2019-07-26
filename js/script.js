d3b120cfda2c706f187ccff8662f3d3be4e29070
stacy murenzi <stacymurenzi@gmail.com>
2019-07-26 21:22:04 +0200

    changes

--- js/script.js
+++ js/script.js
@@ -5,9 +5,8 @@ $(document).ready(function() {
     $("#1stparagraph").hide();
     $("#2paragraph").hide();
     $("#3paragraph").hide();
-});
 
-$(document).ready(function() {
+
     $("#pic1,label").click(function() {
         $("#pic1").hide();
         $("label").hide();
@@ -21,9 +20,7 @@ $(document).ready(function() {
         $("#pic1").show();
         $("label").show();
     });
-});
 
-$(document).ready(function() {
     $("#dev").click(function() {
         $("#dev").hide();
         $("label").hide();
@@ -36,8 +33,7 @@ $(document).ready(function() {
         $("#dev").show();
         $("label").show();
     });
-});
-$(document).ready(function() {
+
     $("#product").click(function() {
         $("#product").hide();
         $("label").hide();
@@ -50,4 +46,87 @@ $(document).ready(function() {
         $("#product").show();
         $("label").show();
     });
+
 });
+$(document).ready(function() {
+    $("#p1").hide();
+    $("#prt1").animate({ opacity: 5 });
+    $("#prt1").hover(function() {
+        $(this).stop().animate({ opacity: 0.3 });
+        $("#p1").fadeIn();
+    }, function() {
+        $(this).stop().animate({ opacity: 1 });
+        $("#p1").fadeOut();
+
+    });
+    $("#p2").hide();
+    $("#prt2").animate({ opacity: 5 });
+    $("#prt2").hover(function() {
+        $(this).stop().animate({ opacity: 0.3 });
+        $("#p2").fadeIn();
+    }, function() {
+        $(this).stop().animate({ opacity: 1 });
+        $("#p2").fadeOut();
+
+    });
+    $("#p3").hide();
+    $("#prt3").animate({ opacity: 5 });
+    $("#prt3").hover(function() {
+        $(this).stop().animate({ opacity: 0.3 });
+        $("#p3").fadeIn();
+    }, function() {
+        $(this).stop().animate({ opacity: 1 });
+        $("#p3").fadeOut();
+
+    });
+    $("#p4").hide();
+    $("#prt4").animate({ opacity: 5 });
+    $("#prt4").hover(function() {
+        $(this).stop().animate({ opacity: 0.3 });
+        $("#p4").fadeIn();
+    }, function() {
+        $(this).stop().animate({ opacity: 1 });
+        $("#p4").fadeOut();
+
+    });
+    $("#p5").hide();
+    $("#prt5").animate({ opacity: 5 });
+    $("#prt5").hover(function() {
+        $(this).stop().animate({ opacity: 0.3 });
+        $("#p5").fadeIn();
+    }, function() {
+        $(this).stop().animate({ opacity: 1 });
+        $("#p5").fadeOut();
+
+    });
+    $("#p6").hide();
+    $("#prt6").animate({ opacity: 5 });
+    $("#prt6").hover(function() {
+        $(this).stop().animate({ opacity: 0.3 });
+        $("#p6").fadeIn();
+    }, function() {
+        $(this).stop().animate({ opacity: 1 });
+        $("#p6").fadeOut();
+
+    });
+    $("#p7").hide();
+    $("#prt7").animate({ opacity: 5 });
+    $("#prt7").hover(function() {
+        $(this).stop().animate({ opacity: 0.3 });
+        $("#p7").fadeIn();
+    }, function() {
+        $(this).stop().animate({ opacity: 1 });
+        $("#p7").fadeOut();
+
+    });
+    $("#p8").hide();
+    $("#prt8").animate({ opacity: 5 });
+    $("#prt8").hover(function() {
+        $(this).stop().animate({ opacity: 0.3 });
+        $("#p8").fadeIn();
+    }, function() {
+        $(this).stop().animate({ opacity: 1 });
+        $("#p8").fadeOut();
+
+    });
+})
